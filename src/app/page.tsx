6a97bf84d111c58ee15ee19a12ba3d4c7df97a8c
zczhao1992 <zczhao1992@163.com>
2026-06-25 20:23:29 +0800

    feat: 完成卷积层图形展示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { Progress } from "@/components/ui/progress";
 import { ESC50_EMOJI_MAP, ESC50_CN_MAP } from "./config";
 import ColorScale from "@/components/ColorScale";
 import FeatureMap from "@/components/FeatureMap";
+import Waveform from "@/components/Waveform";
 
 interface Prediction {
   class: string;
@@ -212,7 +213,61 @@ export default function HomePage() {
                   </div>
                 </CardContent>
               </Card>
+
+              <Card>
+                <CardHeader>
+                  <CardTitle className="text-stone-900">音频波形</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <Waveform
+                    data={vizData.waveform.values}
+                    title={`${vizData.waveform.duration.toFixed(2)}s * ${vizData.waveform.sample_rate}Hz`}
+                  />
+                </CardContent>
+              </Card>
             </div>
+
+            <Card>
+              <CardHeader>
+                <CardTitle>卷积层输出</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="grid grid-cols-5 gap-6">
+                  {main.map(([mainName, mainData]) => (
+                    <div key={mainName} className="space-y-4">
+                      <div>
+                        <h4 className="mb-2 font-medium text-stone-700">
+                          {mainName}
+                        </h4>
+                        <FeatureMap
+                          data={mainData.values}
+                          title={`${mainData.shape.join(" x ")}`}
+                        />
+                      </div>
+                      {internals[mainName] && (
+                        <div className="h-80 overflow-y-auto rounded border border-stone-200 bg-stone-50 p-2">
+                          <div className="space-y-2">
+                            {internals[mainName]
+                              .sort(([a], [b]) => a.localeCompare(b))
+                              .map(([layerName, layerData]) => (
+                                <FeatureMap
+                                  key={layerName}
+                                  data={layerData.values}
+                                  title={layerName.replace(`${mainName}.`, "")}
+                                  internal={true}
+                                />
+                              ))}
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  ))}
+                </div>
+                <div className="mt-5 flex justify-end">
+                  <ColorScale width={200} height={16} min={-1} max={1} />
+                </div>
+              </CardContent>
+            </Card>
           </div>
         )}
       </div>
